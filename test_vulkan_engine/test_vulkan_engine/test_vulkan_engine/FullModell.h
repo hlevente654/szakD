a7741a3f8f9f835bc79e19ff8d919bd1ca2c9e45
hlevente654 <hlevente654@gmail.com>
2025-05-22 21:06:47 +0200

    last update for now

--- test_vulkan_engine/test_vulkan_engine/test_vulkan_engine/FullModell.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class FullModell
-{
-public:
-	std::vector<MeshModel> modelList;
-
-};
-
--- test_vulkan_engine/test_vulkan_engine/test_vulkan_engine/ModelControll.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class ModelControll
-{
-public:
-	int modelId;
-
-	void keyControl(bool* keys, float deltaTime, float angleSpeed);
-};
-
